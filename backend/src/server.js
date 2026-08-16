9a0a524c9a1a412ab66ca0ab04162101dca95c22
haruwoso20 <louis.sharon19@gmail.com>
2026-08-16 12:15:47 +0530

    connected frontend and backend on the same port

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,28 +4,36 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import ratelimiter from "./middleware/ratelimiter.js";
 import cors from "cors"
-
+import path from "path"
  
 dotenv.config(); 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware 
+if(process.env.NODE_ENV !=="production"){
 app.use(
     cors({
     origin:"http://localhost:5173",
 })
 );
+}
 app.use(express.json());//this middleware will parse JSON bodies:req.body
-app.use(ratelimiter);
-
-
+app.use("/api", ratelimiter);
 app.use("/api/notes", routes);
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+}
+
+
 
 connectDB().then(()=>{
 app.listen(PORT,()=>{
 console.log("Server started on port :",PORT);
 });
 });
-
 
